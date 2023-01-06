7dee527a8b7fadb7b09463c98a899e6b41dba2f1
Rhythmicc <RhythmLian@outlook.com>
2023-01-06 12:13:28 +0800

    update for Qpro

--- MtxDrawer/main.py
+++ MtxDrawer/main.py
@@ -2,12 +2,19 @@ from QuickProject.Commander import Commander
 from . import console, info_string
 from .Drawer import Drawer
 
-app = Commander(True)
-rt_path = './'
+app = Commander("mtx-drawer", True)
+rt_path = "./"
 
 
 @app.command()
-def draw_one(filepath: str, ops: list, force: bool = False, log_times: int = 2, mat_size: int = 200, block_size: int = -1):
+def draw_one(
+    filepath: str,
+    ops: list,
+    force: bool = False,
+    log_times: int = 2,
+    mat_size: int = 200,
+    block_size: int = -1,
+):
     """
     单个文件处理
     :param filepath: 文件路径
@@ -19,7 +26,14 @@ def draw_one(filepath: str, ops: list, force: bool = False, log_times: int = 2, 
     :return:
     """
     try:
-        drawer = Drawer(filepath, 'aver' in ops, force, log_times, set_mat_size=mat_size, set_block_size=block_size)
+        drawer = Drawer(
+            filepath,
+            "aver" in ops,
+            force,
+            log_times,
+            set_mat_size=mat_size,
+            set_block_size=block_size,
+        )
     except ValueError:
         return
     for func in ops:
@@ -27,7 +41,13 @@ def draw_one(filepath: str, ops: list, force: bool = False, log_times: int = 2, 
 
 
 @app.command()
-def draw(ops: list, force: bool = False, log_times: int = 2, mat_size: int = 200, block_size: int = -1):
+def draw(
+    ops: list,
+    force: bool = False,
+    log_times: int = 2,
+    mat_size: int = 200,
+    block_size: int = -1,
+):
     """
     多个文件处理
     :param ops: 操作列表
@@ -39,17 +59,21 @@ def draw(ops: list, force: bool = False, log_times: int = 2, mat_size: int = 200
     """
     import os
 
-    has_aver = 'aver' in ops
+    has_aver = "aver" in ops
     for rt, sonDirs, sonFiles in os.walk(rt_path, followlinks=True):
-        if not rt.endswith('/'):
-            rt += '/'
+        if not rt.endswith("/"):
+            rt += "/"
         for file in sonFiles:
-            if file.endswith('.mtx'):
+            if file.endswith(".mtx"):
                 try:
                     console.print(info_string, f'正在处理: "{rt + file}"')
                     drawer = Drawer(
-                        rt + file, has_aver, force, log_times,
-                        set_mat_size=mat_size, set_block_size=block_size
+                        rt + file,
+                        has_aver,
+                        force,
+                        log_times,
+                        set_mat_size=mat_size,
+                        set_block_size=block_size,
                     )
                     for func in ops:
                         drawer.call(func)
@@ -58,12 +82,16 @@ def draw(ops: list, force: bool = False, log_times: int = 2, mat_size: int = 200
                 except Exception:
                     console.print_exception()
                 else:
-                    console.print(info_string, '处理完成')
+                    console.print(info_string, "处理完成")
                 finally:
-                    console.print('-' * console.width)
-    console.print(info_string, '处理完成')
-    console.print('-' * console.width)
+                    console.print("-" * console.width)
+    console.print(info_string, "处理完成")
+    console.print("-" * console.width)
 
 
 def main():
     app()
+
+
+if __name__ == "__main__":
+    main()
